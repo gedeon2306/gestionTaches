d153c8fbcaa85f5f2487c49901f1a377e8735bf1
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-05-07 22:12:12 -1200

    Model UserManager

--- backend/task/models.py
+++ backend/task/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+# Manager pour gérer la création des utilisateurs
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("L'email est obligatoire")
+        user = self.model(email=self.normalize_email(email), name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
