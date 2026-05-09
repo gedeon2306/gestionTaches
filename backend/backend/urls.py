7dab49928946fb77aa777cf61f18b4a14965a043
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-05-09 17:34:59 +0100

    Installation de nextAuth et configaration des clés OAuth

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
-from api.views import landing_view
+from task.views import landing_view
 
 urlpatterns = [
     path("", landing_view, name="landing"),
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),
+    path("api/", include("task.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
