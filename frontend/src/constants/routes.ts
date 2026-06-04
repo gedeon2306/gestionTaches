848d081a82b0e70fcc807b7645c22a7104e909a3
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-06-03 22:04:34 -1200

    refactor(frontend): routes.ts

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -1,4 +1,11 @@
 export const ROUTES = {
   HOME: '/',
-  
+  AUTH: {
+    LOGIN: '/auth/login',
+    REGISTER: '/auth/register',
+    EMAIL_SEND: '/auth/email-send',
+    CONFIRM: '/auth/confirm',
+    FORGOT_PASSWORD: '/auth/forgot-password',
+    RESET_PASSWORD: '/auth/reset-password',
+  },
 } as const;
