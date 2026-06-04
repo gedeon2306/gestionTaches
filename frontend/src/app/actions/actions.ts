6b5abd68bea7f716b823007b458ec542be8a75b8
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-06-03 22:01:39 -1200

    refactor(frontend): actions.ts

--- frontend/src/app/actions/actions.ts
+++ frontend/src/app/actions/actions.ts
@@ -3,3 +3,27 @@ import api from '@/src/constants/api';
 import { cookies } from 'next/headers';
 
 
+async function refreshAccessToken(): Promise<string | null> {
+  const cookieStore = await cookies();
+  const refreshToken = cookieStore.get('refresh_token')?.value;
+
+  if (!refreshToken) return null;
+
+  try {
+    const response = await api.post('token/refresh/', { refresh: refreshToken });
+    const newAccessToken = response.data.access;
+
+    cookieStore.set('access_token', newAccessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      path: '/',
+      maxAge: 60 * 60 * 24
+    });
+
+    return newAccessToken;
+  } catch (error) {
+    console.error('Échec du renouvellement du token:', error);
+    return null;
+  }
+}
