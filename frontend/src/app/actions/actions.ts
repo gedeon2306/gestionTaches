6521d4e951f6c0677f27268b4e0020221b4fb10d
GANGOUE MBIMI jihrel Gédéon <gedeon.jirehl.gangoue23@gmail.com>
2026-06-03 22:02:09 -1200

    refactor(frontend): actions.ts

--- frontend/src/app/actions/actions.ts
+++ frontend/src/app/actions/actions.ts
@@ -34,3 +34,9 @@ const authConfig = (token: string) => ({
 });
  
 
+async function getToken(): Promise<string> {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("access_token")?.value;
+  if (!token) throw new Error("Non authentifié");
+  return token;
+}
